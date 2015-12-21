02668f8a7efb2a4d661385d6eaa24401f911e8f8
EC2 Default User <david.zipkin@gmail.com>
2015-12-21 04:09:13 +0000

    misc changes

--- TempRead.py
+++ TempRead.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 import random
+import time
 
 def readTemp(): #program does nothing as written
     return  random.uniform (1,10)
@@ -16,16 +17,25 @@ channel = connection.channel()
 
 channel.queue_declare(queue='temp_queue', durable=True)
 
-temp = readTemp ()
+while True: 
+    
+    temp = readTemp ()
+    message = {}
 
-message = "Temp of " + str(temp) + "added." 
-channel.basic_publish(exchange='',
+    message ['temp'] = str(temp)
+    message ['time'] = str(time.time())
+    message ['sensor'] = "sensorID"
+    message ['type'] = "temperature"
+
+    channel.basic_publish(exchange='',
                       routing_key='task_queue',
-                      body=message,
+                      body=str(message),
                       properties=pika.BasicProperties(
                          delivery_mode = 2, # make message persistent
                       ))
-print " [x] Sent %r" % (message,)
+    print " [x] Sent %r" % (message,)
+    time.sleep (10)
+
 connection.close()
 
 def readTemp(): #program does nothing as written
--- TempSend.py
+++ TempSend.py
@@ -2,6 +2,16 @@
 import pika
 import time
 
+def sendIoTMessage (message)
+    
+    command = "mosquitto_pub --cafile rootCA.pem --cert cert.pem --key privateKey.pem -h A3TNLWZ1LLGHU0.iot.us-east-1.amazonaws.com -p 8883 -q 1 -d -t topic/test2 -i clientid2 -m "
+
+    # make sure that you intrepret the dict the right way
+ast.literal_eval
+    command = command + "\"{\\\"msg\\\" : \\\"Happy hungry\\\", \\\"temperature\\\" : \\\"452\\\"}\""
+
+    os.system (command)
+
 connection = pika.BlockingConnection(pika.ConnectionParameters(
         host='localhost'))
 channel = connection.channel()
@@ -11,6 +21,7 @@ print ' [*] Waiting for messages. To exit press CTRL+C'
 
 def callback(ch, method, properties, body):
     print " [x] Received %r" % (body,)
+    sendIoTMessage (body) 
     time.sleep( body.count('.') )
     print " [x] Done"
     ch.basic_ack(delivery_tag = method.delivery_tag)
