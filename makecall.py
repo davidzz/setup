614b5ec8717945efb7224838bdd9388854bfd3f1
EC2 Default User <david.zipkin@gmail.com>
2015-12-21 03:11:05 +0000

    new

--- makecall.py
+++ makecall.py
@@ -3,8 +3,15 @@ import os
 from subprocess import call
 call(["ls", "-l"])
 
-command = "mosquitto_pub --cafile rootCA.pem --cert cert.pem --key privateKey.pem -h A3TNLWZ1LLGHU0.iot.us-east-1.amazonaws.com -p 8883 -q 1 -d -t topic/test2 -i clientid2 -m \"{\\\"msg\\\" : \\\"Happy hungry\\\"}\""
-#\"{\"key2\" : \"Hello2, World\"}\""
+d = {}
+d['temp'] = "451"
+d['msg'] = "yoyoyo"
+
+print d
+
+command = "mosquitto_pub --cafile rootCA.pem --cert cert.pem --key privateKey.pem -h A3TNLWZ1LLGHU0.iot.us-east-1.amazonaws.com -p 8883 -q 1 -d -t topic/test2 -i clientid2 -m "
+
+command = command + "\"{\\\"msg\\\" : \\\"Happy hungry\\\", \\\"temperature\\\" : \\\"452\\\"}\""
 
 os.system (command)
 
